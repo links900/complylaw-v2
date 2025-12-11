755caf5da6684b37d9a890373fd2e1cde41a8c13
hp202350 <hp_202350@outlook.com>
2025-12-11 19:15:38 +0500

    FINAL routing.py – clean and working

--- scanner/routing.py
+++ scanner/routing.py
@@ -2,11 +2,7 @@
 from django.urls import re_path
 from . import consumers
 
-
 websocket_urlpatterns = [
-    # Live scan progress
     re_path(r'ws/scan/(?P<scan_id>[\w-]+)/$', consumers.ScanProgressConsumer.as_asgi()),
-    
-    # Global notifications (Grade A confetti, toasts, etc.)
     re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
 ]
