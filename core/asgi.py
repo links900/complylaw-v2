bedca8f35b2c022e600eae1abc9b9f8eb84c486d
hp202350 <hp_202350@outlook.com>
2025-12-11 19:54:09 +0500

    Remove WebSocket URLs from core.urls and fix ASGI routing

--- core/asgi.py
+++ core/asgi.py
@@ -3,20 +3,17 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import scanner.routing
+from django.urls import re_path
+from scanner.consumers import ScanProgressConsumer, NotificationConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Create the Django ASGI application once
-django_asgi_app = get_asgi_application()
-
-# Final ASGI application with WebSockets
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            scanner.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            re_path(r'ws/scan/(?P<scan_id>[\w-]+)/$', ScanProgressConsumer.as_asgi()),
+            re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
+        ])
     ),
 })
